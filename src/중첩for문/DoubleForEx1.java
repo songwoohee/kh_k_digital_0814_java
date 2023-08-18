99ab4c7a7a767fd0dd6e5bf5dfcc1c14a6d62815
songwoohee <songwoohee@naver.com>
2023-08-18 14:11:20 +0900

    중첩for문

---
+++ src/중첩for문/DoubleForEx1.java
@@ -0,0 +1,23 @@
+package 중첩for문;
+// 이중 반복문 :
+public class DoubleForEx1 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.printf("|i=%d|", i);
+            for(int j = 0; j < 10; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+//         구구단
+//
+//        for (int i = 2; i < 10; i++) {
+//            for (int j = 1; j< 10; j++) {
+//                System.out.printf("%d x %d = %d\n ", i, j, (i*j));
+//            }
+//            System.out.println();
+//        }
+
+    }
+}
