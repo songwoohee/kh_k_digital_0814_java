e20f94ece7138c2eee887b5503357c5ff5b3fa48
songwoohee <songwoohee@naver.com>
2023-08-18 15:22:07 +0900

    별찍기연습

---
+++ src/별찍기연습/StarEx1.java
@@ -0,0 +1,34 @@
+package 별찍기연습;
+
+import java.util.Scanner;
+
+// 삼각형 별찍기
+// 입력 받은 수만큼 별찍기
+// 5를 입력 하면
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+public class StarEx1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("별 개수 입력 : ");
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i+1 ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
+// 출력 예시
+//별 개수 입력 : 5
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
