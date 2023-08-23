8400e7520df960353a2b2540944e1a97c0166286
songwoohee <songwoohee@naver.com>
2023-08-23 17:46:19 +0900

    객체타입확인 instanceof 기본 예제

---
+++ src/객체타입확인/InstanceOf.java
@@ -0,0 +1,30 @@
+package 객체타입확인;
+// instanceof : 상속 관계가 성립하는지 확인하는 용도
+// 왼쪽 피연산자가 오른쪽 피연산자의 인스턴스인지를 검사하며
+// 결과는 불리언 값(true 또는 false)으로 반환됩니다.
+// boolean result = 좌향(객체) instanceof 우향(타입)
+public class InstanceOf {
+    public static void main(String[] args) {
+        Parent parent = new Parent("부모");
+        Child child = new Child("자식");
+        System.out.println(parent instanceof Parent ); // true
+        System.out.println(child instanceof Child); // true
+        System.out.println(parent instanceof Child); // false
+        System.out.println(child instanceof Child); // true
+
+    }
+}
+class Parent {
+    String name;
+    Parent(String name) {
+        this.name = name;
+    }
+}
+class Child extends Parent {
+
+    Child(String name) {
+        super("부모");
+        this.name = name;
+
+    }
+}
