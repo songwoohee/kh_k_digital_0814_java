3f9d6d702629053c5d42f6c30841902c0bc34f44
songwoohee <songwoohee@naver.com>
2023-08-24 17:07:52 +0900

    getter&setter 예시

---
+++ src/게터와세터/CarTest.java
@@ -0,0 +1,55 @@
+package 게터와세터;
+
+public class CarTest {
+    String modelName;
+    String manufacture;
+    String color;
+    String year;
+    int maxSpeed;
+
+    public CarTest(String modelName, String manufacture, String color) {
+        this.modelName = modelName;
+        this.manufacture = manufacture;
+        this.color = color;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
