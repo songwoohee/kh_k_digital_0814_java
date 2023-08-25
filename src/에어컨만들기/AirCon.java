dcb5bcd63aabae0d764e1bca0e7da1118753ad91
songwoohee <songwoohee@naver.com>
2023-08-25 11:02:26 +0900

    에어컨만들기 (클래스 기능을 통해 사물 구현,필드와 메소드로 구현실행)

---
+++ src/에어컨만들기/AirCon.java
@@ -0,0 +1,89 @@
+package 에어컨만들기;
+/*
+전원 : ON / OFF
+온도 설정 기능
+현재 온도 표시 기능
+Cooler ON / OFF
+Heater ON / OFF
+바람세기 : 1단 / 2단 / 3단
+ */
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class AirCon {
+    private boolean isPower;   // ON / OFF , 선택값이 2가지만 있는 경우 boolean 쓰는게 best, 아니면 int로 써도 됨(0(f)/1(t)로 구분)
+    private int setTemp;   // 온도 설정
+    private int currTemp;  //  현재 온도
+    private boolean isCooler; // 냉방기
+    private boolean isHeater; // 난방기
+    private int windStep;  // 1단, 2단, 3단
+    AirCon() {   // 생성자 생성
+        // 에어컨의 현재 온도를 보여주기 위한 값
+        final int[] MONTHTEMP = {-5, 3, 10, 15, 22, 28, 32, 30, 24, 16, 8, 2};
+        Calendar now = Calendar.getInstance();
+        int month = now.get(Calendar.MONTH);  // MONTHTEMP 에 있는 배열의 인덱스와 동일
+        currTemp = MONTHTEMP[month];   // 배열에 설정한 온도를 가져옴 , 달에 대한 정보를 가져와줘 ex. 7을 넣으면 32도를 출력
+        isPower = false;
+        setTemp = 18;
+        isCooler = false;
+        isHeater = false;
+        windStep = 1;
+    }
+    void airConInfo() {
+        String onOffStr = (isPower) ? "ON" : "OFF";
+        String heaterStr = (isHeater) ? "ON" : "OFF";
+        String coolerStr = (isCooler) ? "ON" : "OFF";
+        final String[] windStr = {"", "1단계", "2단계", "3단계"};
+        System.out.println("=".repeat(10) + "에어컨 정보" + "=".repeat(10));
+        System.out.println("전원" + onOffStr);
+        System.out.println("현재 온도 : " + currTemp);
+        System.out.println("설정 온도 : " + setTemp);
+        System.out.println("히터 : " + heaterStr);
+        System.out.println("클러 : " + coolerStr);
+        System.out.println("바람세기 : " + windStr[windStep]);
+    }
+    public void setAirconState() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("현재 온도는 " + currTemp + " 입니다.");
+        System.out.print("온도를 설정 하세요. : ");
+        setTemp = sc.nextInt();
+        System.out.println("설정 온도는 " + setTemp + " 입니다.");
+        System.out.print("바람 세기를 설정 하세요. (1단/2단/3단) : ");
+        windStep = sc.nextInt();
+        if (currTemp > setTemp) {  // 설정 온도가 현재 온도 보다 낮은 상태. 즉, 더운 상태
+            System.out.println("Cooler가 동작 합니다.");
+            isCooler = true;
+            isHeater = false;  // 혹시 모르니 히터도 꺼주는 동작을 해줌.
+        } else if (currTemp < setTemp) { // 설정 온도가 현재 온도보다 높은 상태. 즉, 추운 상태
+            isCooler = false;
+            isHeater = true;
+        } else {
+            isCooler = false;
+            isHeater = false;
+        }
+
+    }
+    public void setPower(boolean power) {
+        isPower = power;
+    }
+    public int getWindStep() {
+        return windStep;
+    }
+    public boolean isHeater() {
+        return isHeater;
+    }
+    public boolean isCooler() {
+        return isCooler;
+    }
+    public void setCurrTemp(int n) {  // 매개변수의 정수값을 받아 현재 온도를 변경해 줌
+        currTemp += n;
+    }
+    public int getCurrTemp() { // 현재 온도를 읽는 메소드
+        return currTemp;
+    }
+    public int getSetTemp() { // 설정 온도를 읽는 메소드
+        return setTemp;
+    }
+
+}
---
+++ src/에어컨만들기/AirconMain.java
@@ -0,0 +1,44 @@
+package 에어컨만들기;
+
+import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
+
+public class AirconMain {
+    public static void main(String[] args) throws InterruptedException {
+        AirCon lgAirCon = new AirCon();
+        int elapsedTime = 0;  // 경과 시간을 계산하기 위한 변수
+        boolean isSetTemp = false; // 온도를 변경할 조건인지 판단 하는 변수
+        Scanner sc = new Scanner(System.in);
+        System.out.print("에어컨을 켜시겠습니까? ( yes / no ) : ");
+        String isOn = sc.next();
+        if(isOn.equalsIgnoreCase("yes")) {
+            lgAirCon.setPower(true);
+            lgAirCon.setAirconState();
+            while (true) {
+                sleep(1000);
+                elapsedTime++;
+                switch (lgAirCon.getWindStep()) {
+                    case 1:  if(elapsedTime >= 60) isSetTemp = true; break; // 1단
+                    case 2:  if(elapsedTime >= 30) isSetTemp = true; break; // 2단
+                    case 3 : if(elapsedTime >= 20) isSetTemp = true; break; // 3단
+                    default: if(elapsedTime >= 61) isSetTemp = true;
+                }
+                if(isSetTemp) {
+                    if(lgAirCon.isHeater()) lgAirCon.setCurrTemp(1);
+                    if(lgAirCon.isCooler()) lgAirCon.setCurrTemp(-1);
+                    lgAirCon.airConInfo();
+                    isSetTemp = false; // 온도 변경 이후 초기화
+                    elapsedTime = 0;   // 온도 변경 이후 초기화
+                    }
+                if(lgAirCon.getCurrTemp() == lgAirCon.getSetTemp()) {
+                    System.out.println("에어컨을 종료 합니다.");
+                    break;
+                }
+
+            }
+        }
+
+
+    }
+}
