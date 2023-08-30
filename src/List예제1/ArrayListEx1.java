7dcabfda59b6927043013ad46b8882197b49308d
songwoohee <songwoohee@naver.com>
2023-08-30 14:18:27 +0900

    List예제1-최종(주석 추가)

--- src/List예제1/ArrayListEx1.java
+++ src/List예제1/ArrayListEx1.java
@@ -5,12 +5,22 @@ import java.util.List;
 
 public class ArrayListEx1 {
     public static void main(String[] args) {
-        List<MenuInfo> menuInfo = new ArrayList<>();
-        menuInfo.add(new MenuInfo("Americano", 2500,"coffee",true));
-        menuInfo.add(new MenuInfo("Latte", 4000,"coffee",true));
-        menuInfo.add(new MenuInfo("Blacktea", 4000,"tea",true));
-        for(MenuInfo menu : menuInfo) {
+        // List 인터페이스의 참조변수로 ArrayList 객체를 참조 함
+        List<MenuInfo> menuInfoList = new ArrayList<>();
+        // add는 객체를 추가, 추가 할때마다 크기가 변함
+        MenuInfo menuInfo1 = new MenuInfo("Americano", 2500,"coffee",true);
+        menuInfoList.add(menuInfo1);   // 참조 변수를 만들어서 넣어주나 객체를 바로 넣어주나 결과는 동일
+        // 위 아래는 같은 내용을 출력 한다.
+        menuInfoList.add(new MenuInfo("Americano", 2500,"coffee",true));
+        menuInfoList.add(new MenuInfo("Latte", 4000,"coffee",true));
+        menuInfoList.add(new MenuInfo("Blacktea", 4000,"tea",true));
+        // 향상된 for문 : 배열이나 Collection 클래스 자식들
+        for(MenuInfo menu : menuInfoList) {
             System.out.println(menu.getMenuInfo());
+        }
+        // 기본 for문 , 위 아래 같은 내용 출력
+        for (int i = 0; i < menuInfoList.size(); i++) {
+            System.out.println(menuInfoList.get(i).getMenuInfo());
         }
 
     }
