9c08f06917a6bdca71746677534195f144ad9b49
songwoohee <songwoohee@naver.com>
2023-09-01 17:19:53 +0900

    쓰레드예제5번 setDaemon 보조 쓰레드

---
+++ src/쓰레드예제5번/ThMainEx5.java
@@ -0,0 +1,34 @@
+package 쓰레드예제5번;
+
+import static java.lang.Thread.sleep;
+
+/*
+데몬쓰레드 : 다른 쓰레드의 작업을 돕는 보조 쓰레드, 데몬 쓰레드 이외의 쓰레드가 모두 종료 되면 자동 종료
+ */
+public class ThMainEx5 {
+    public static void main(String[] args) throws InterruptedException {
+        AutoSaveTh autoSaveTh = new AutoSaveTh();
+        autoSaveTh.setDaemon(true);    // 데몬 쓰레드 만드는 방법 , 다른 쓰레드가 종료되면 같이 종료
+        autoSaveTh.start();
+        sleep(30000);
+
+    }
+}
+
+class AutoSaveTh extends Thread {   // AutoSaveTh 자동 저장
+    public void save() {
+        System.out.println("작업 내용을 저장 함");
+    }
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            save();
+            }
+        }
+    }
+
